463750e426e1b7b8ab99434ab9e8c2f892a55340
alexandregama <alexandre.gama.lima@gmail.com>
2015-07-14 21:52:22 -0300

    Adiciona pesquisa de projetos por Query param

--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import br.com.alura.loja.dao.HashMapProjetoDao;
@@ -14,10 +15,10 @@ public class ProjetoResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public String busca() {
+	public String busca(@QueryParam("id") Long id) {
 		Projetos projetos = new HashMapProjetoDao();
 		
-		Projeto projeto = projetos.busca(1L);
+		Projeto projeto = projetos.busca(id);
 		
 		return projeto.toXml();
 	}
